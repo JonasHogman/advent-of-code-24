246d3112ac9902d055737b3c35802e95779aa4dd
Jonas Hogman <jonas.hogman@seb.se>
2024-12-28 17:50:54 +0000

    Add two-star solution for day 11

--- day-11/main.go
+++ day-11/main.go
@@ -3,23 +3,44 @@ package main
 import (
 	"bufio"
 	"log"
+	_ "net/http/pprof"
 	"os"
+	"runtime/pprof"
 	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	if err := pprof.StartCPUProfile(f); err != nil {
+		log.Fatal(err)
+	}
+	defer pprof.StopCPUProfile()
+
 	start := time.Now()
 
 	ints := getInput()
-	log.Printf("Input: %v", ints)
-	for i := 0; i < 25; i++ {
-		ints = blink(ints)
+	freqs := make(map[int]int)
+	results := make(map[int][]int)
+	for _, i := range ints {
+		freqs[i] = 1
 	}
-
-	stones := len(ints)
-	log.Printf("Stones: %d", stones)
+	log.Printf("Input: %v", ints)
+	for i := 0; i < 75; i++ {
+		freqs, results = blink(freqs, results)
+		log.Printf("Iteration: %v", i)
+		sum := 0
+		for _, v := range freqs {
+			sum += v
+		}
+		log.Printf("Stones: %v", sum)
+	}
 
 	duration := time.Since(start)
 	log.Printf("Execution time: %s", duration)
@@ -39,30 +60,84 @@ func getInput() []int {
 		b := []int{}
 		for _, v := range a {
 			in, _ := strconv.Atoi(v)
-			b = append(b, in)
+			b = append(b, int(in))
 		}
 		return b
 	}
 	return nil
 }
 
-func blink(ints []int) []int {
-	newInts := []int{}
-	for _, v := range ints {
-		if v == 0 {
-			newInts = append(newInts, 1)
-		} else {
-			s := strconv.Itoa(v)
-			l := len(s)
-			if l%2 == 0 {
-				firstHalf, _ := strconv.Atoi(s[:l/2])
-				lastHalf, _ := strconv.Atoi(s[l/2:])
-				newInts = append(newInts, firstHalf)
-				newInts = append(newInts, lastHalf)
+func blink(freqs map[int]int, results map[int][]int) (map[int]int, map[int][]int) {
+	newFreqs := make(map[int]int)
+	for k, v := range freqs {
+		result, ok := results[k]
+		if !ok {
+			result = calc(k)
+			results[k] = result
+		}
+		for _, r := range result {
+			existingFreq, ok := newFreqs[r]
+			if !ok {
+				newFreqs[r] = v
 			} else {
-				newInts = append(newInts, v*2024)
+				newFreqs[r] = existingFreq + v
 			}
 		}
 	}
-	return newInts
+	return newFreqs, results
+}
+
+func calc(v int) []int {
+	if v == 0 {
+		return []int{1}
+	} else {
+		l := intLength(v)
+		if l%2 == 0 {
+			halfLength := l / 2
+			divisor := intPow(10, halfLength)
+			firstHalf := v / divisor
+			lastHalf := v % divisor
+			return []int{firstHalf, lastHalf}
+		} else {
+			return []int{v * 2024}
+		}
+	}
+}
+
+func intLength(v int) int {
+	length := 0
+	temp := v
+	for temp > 0 {
+		length++
+		temp /= 10
+	}
+	return length
+}
+
+func firstHalf(v int, length int) int {
+	halfLength := length / 2
+	divisor := intPow(10, halfLength)
+	firstHalf := v / divisor
+
+	return firstHalf
+}
+
+func lastHalf(v int, length int) int {
+	halfLength := length / 2
+	divisor := intPow(10, halfLength)
+	lastHalf := v % divisor
+
+	return lastHalf
+}
+
+func intPow(base int, exp int) int {
+	result := 1
+	for exp > 0 {
+		if exp&1 == 1 {
+			result *= base
+		}
+		base *= base
+		exp >>= 1
+	}
+	return result
 }
